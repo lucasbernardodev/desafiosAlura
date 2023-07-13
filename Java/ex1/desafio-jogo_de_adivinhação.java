772367c7ebece5f01b646e123ed762a04ffdc0fd
Lucas Gabriel <105575164+lucasbernardodev@users.noreply.github.com>
2023-07-13 10:49:22 -0300

    :rocket: Arrumação código

--- Java/ex1/desafio-jogo_de_adivinhação.java
+++ Java/ex1/desafio-jogo_de_adivinhação.java
@@ -1,69 +1,53 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class DesafioAlura {
+public class desafioAlura {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int situacaoJogo = -1;
+        System.out.println("Digite 1 para iniciar o jogo ou outro número para sair: ");
+        int situacaoJogo = sc.nextInt();
 
-        while (situacaoJogo != -1) {
-            System.out.println("Digite 1 para iniciar o jogo ou -1 para sair: ");
-            situacaoJogo = scanner.nextInt();
-
-            if (situacaoJogo == 1) {
-                System.out.println("Jogo iniciado!");
-
-                int numeroSorteado = new Random().nextInt(10);
-                int chances = 4;
-
-                for (int i = 0; i < 5; i++) {
-                    System.out.println("Tente adivinhar um número de 1 a 10: ");
-                    int leituraUsuario = scanner.nextInt();
-
-                    if (leituraUsuario == numeroSorteado) {
-                        System.out.println("Parabéns! Você acertou");
-                        break;
-                    } else {
-                        System.out.println("Errado, tente novamente.");
-
-                        if (chances != 0) {
-                            System.out.println("Restam " + chances + " tentativas.");
-                            chances--;
-                        } else {
-                            System.out.println("Você perdeu!");
-                            break;
-                        }
-                    }
-                }
-
-                System.out.println("O número sorteado era: " + numeroSorteado);
-
-                int validadorFinal = 0;
-
-                while (validadorFinal != 1) {
-                    System.out.println("Digite 1 para jogar novamente ou -1 para sair: ");
-                    int decisaoFinal = scanner.nextInt();
-
-                    switch (decisaoFinal) {
-                        case 1:
-                            situacaoJogo = 1;
-                            validadorFinal = 1;
-                            break;
-                        case -1:
-                            situacaoJogo = -1;
-                            validadorFinal = 1;
-                            break;
-                        default:
-                            System.out.println("Número inválido, digite uma opção válida: ");
-                            validadorFinal = 2;
-                            break;
+        while (situacaoJogo == 1) {
+            System.out.println("Jogo iniciado!");
+            int num = new Random().nextInt(10);
+            System.out.println("Tente adivinhar um número de 1 à 10: ");
+            int chances = 4;
+            for (int i = 0; i < 5; i++) {
+                int leituraUsuario = sc.nextInt();
+                if (leituraUsuario == num){
+                    System.out.println("Parabéns!");
+                    System.out.println("Você acertou");
+                    i = 6;
+                } else {
+                    System.out.println("Errado, tente novamente.");
+                    if (chances != 0) {
+                        System.out.println("Restam " + chances + " tentativas.");
+                        chances -= 1;
+                    } else
+                    {
+                        System.out.println("Você perdeu!");
                     }
                 }
             }
+            System.out.println("O número sorteado era: " + num);
+            System.out.println("Digite 1 para jogar novamente ou -1 para sair: ");
+            int validadorFinal = 0;
+            while (validadorFinal != 1){
+                int decisaoFinal = sc.nextInt();
+                switch (decisaoFinal){
+                    case 1:situacaoJogo = 1;
+                        validadorFinal = 1;
+                        break;
+                    case -1:situacaoJogo = -1;
+                        validadorFinal = 1;
+                        break;
+                    default:
+                        System.out.println("Número inválido, digite uma opção válida: ");
+                        validadorFinal = 2;
+                        break;
+                }
+            }
         }
-
-        scanner.close();
-        System.out.println("O jogo foi encerrado.");
     }
 }
